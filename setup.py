36661e6b8610cd0106f234905e86e072ffae70c4
unai <u.alegre@utwente.nl>
2019-09-23 17:10:44 +0200

    Created main structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='brainspy-tasks',
+      version='0.0.0',
+      description='Benchmark tests and tasks for studying the capacity of boron-doped silicon devices and their surrogate models.',
+      url='https://github.com/BraiNEdarwin/brainspy-benchmarks',
+      author='This has adopted part of the BRAINS skynet repository code, which has been cleaned and refactored. Some new tests have been created. The maintainers of the code are Hans-Christian Ruiz Euler and Unai Alegre Ibarra.',
+      author_email='u.alegre@utwente.nl',
+      license='GPL-3.0',
+      packages=find_packages(),
+      install_requires=[
+      'pandas',
+      'numpy',
+      'matplotlib'
+      ],
+      python_requires='~=3.6',
+      zip_safe=False)
