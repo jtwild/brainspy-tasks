cf943c98cafa3babc619c6f687e2cec7146ed017
unai <u.alegre@utwente.nl>
2019-10-18 09:40:46 +0200

    Major changes to the capacity test. Now working for the GA. Not working for GD.

--- bspytasks/benchmarks/capacity/capacity_test.py
+++ bspytasks/benchmarks/capacity/capacity_test.py
@@ -5,9 +5,8 @@ Created on Thu Nov 15 16:32:25 2018
 This script generates all binary assignments of N elements.
 @author: hruiz and ualegre
 """
-
 from bspytasks.benchmarks.capacity.vc_dimension_test import VCDimensionTest
-
+from bspytasks.utils.excel import ExcelFile
 from bspyalgo.utils.io import save, load_configs
 
 import numpy as np
@@ -18,101 +17,40 @@ class CapacityTest():
     def __init__(self, configs):
         self.configs = configs
         self.current_dimension = configs['from_dimension']
-        self.threshold = self.__calculate_threshold()
-        self.vcdimension_test = VCDimensionTest(configs['vcdim_configs'])
+        configs['algorithm_configs']['results_base_dir'] = configs['results_base_dir']
+        self.excel_file = ExcelFile(configs['results_base_dir'] + 'capacity_test_results.xlsx')
+        self.vcdimension_test = VCDimensionTest(configs, self.excel_file)
 
     def run_test(self):
         results = {}
+
         while True:
-            self.__init_test()
+            print('==== VC Dimension %d ====' % self.current_dimension)
+            self.vcdimension_test.init_test(self.current_dimension)
             opportunity = 0
             not_found = np.array([])
             while True:
-                test_data = self.vcdimension_test.run_test(binary_labels=not_found)
-                not_found = test_data['gate'].loc[test_data['found'] == False]  # noqa: E712
+                self.vcdimension_test.run_test(binary_labels=not_found)
+                not_found = self.vcdimension_test.get_not_found_gates()
                 opportunity += 1
                 if (not_found.size == 0) or (opportunity >= self.configs['max_opportunities']):
                     break
-            results[str(self.current_dimension)] = self.vcdimension_test.close_test(self.threshold, self.current_dimension, self.configs['show_plot'])
+            results[str(self.current_dimension)] = self.vcdimension_test.close_test()
             if not results[str(self.current_dimension)] or not self.next_vcdimension():
-                self.vcdimension_test.writer.save()
-                save(mode='configs', path=self.configs['vcdim_configs']['output_dir'], filename='test_configs.json', data=self.configs)
-                save(mode='configs', path=self.configs['vcdim_configs']['output_dir'], filename='algorithm_configs.json', data=self.vcdimension_test.algorithm.config_dict)
-                self.results = results
-                return results
+                return self.close_test(results)
 
-    def __init_test(self):
-        print('==== VC Dimension %d ====' % self.current_dimension)
-        inputs = self.generate_test_inputs(self.current_dimension)
-        binary_labels = self.__generate_binary_target(self.current_dimension).tolist()
-        self.threshold = self.__calculate_threshold()
-        self.vcdimension_test.init_data(inputs, binary_labels, self.threshold)
-
-    def __calculate_threshold(self):
-        return 1 - (self.configs['threshold_parameter'] / self.current_dimension)
+    def close_test(self, results):
+        self.excel_file.save_file()
+        save(mode='configs', path=self.configs['results_base_dir'], filename='test_configs.json', data=self.configs)
+        self.results = results
+        return results
 
     def next_vcdimension(self):
         if self.current_dimension + 1 > self.configs['to_dimension']:
             return False
         else:
             self.current_dimension += 1
-            self.__calculate_threshold()
             return True
-
-    # @todo change generation of inputs to differetn vc dimensions
-
-    def generate_test_inputs(self, vc_dim):
-        # @todo create a function that automatically generates non-linear inputs
-        try:
-            if vc_dim == 4:
-                return [[-0.7, -0.7, 0.7, 0.7], [0.7, -0.7, 0.7, -0.7]]
-            elif vc_dim == 5:
-                return [[-0.7, -0.7, 0.7, 0.7, -0.35],
-                        [0.7, -0.7, 0.7, -0.7, 0.0]]
-            elif vc_dim == 6:
-                return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35],
-                        [0.7, -0.7, 0.7, -0.7, 0.0, 0.0]]
-            elif vc_dim == 7:
-                return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35, 0.0],
-                        [0.7, -0.7, 0.7, -0.7, 0.0, 0.0, 1.0]]
-            elif vc_dim == 8:
-                return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35, 0.0, 0.0],
-                        [0.7, -0.7, 0.7, -0.7, 0.0, 0.0, 1.0, -1.0]]
-            else:
-                raise VCDimensionException()
-        except VCDimensionException:
-            print(
-                'Dimension Exception occurred. The selected VC Dimension is %d Please insert a value between ' %
-                vc_dim)
-
-    def __generate_binary_target(self, target_dim):
-        # length of list, i.e. number of binary targets
-        binary_target_no = 2**target_dim
-        assignments = []
-        list_buf = []
-
-        # construct assignments per element i
-        print('===' * target_dim)
-        print('ALL BINARY LABELS:')
-        level = int((binary_target_no / 2))
-        while level >= 1:
-            list_buf = []
-            buf0 = [0] * level
-            buf1 = [1] * level
-            while len(list_buf) < binary_target_no:
-                list_buf += (buf0 + buf1)
-            assignments.append(list_buf)
-            level = int(level / 2)
-
-        binary_targets = np.array(assignments).T
-        print(binary_targets)
-        print('===' * target_dim)
-        return binary_targets
-
-
-class VCDimensionException(Exception):
-    """Exception: It does not exist an implementation of such VC Dimension."""
-    pass
 
 
 if __name__ == '__main__':
@@ -148,7 +86,7 @@ if __name__ == '__main__':
     # capacity_test_configs['max_opportunities'] = 3
     # capacity_test_configs['threshold_parameter'] = 0.5
     # capacity_test_configs['show_plot'] = False
-    capacity_test_configs = load_configs('configs/benchmark_tests/capacity_test/capacity_test_template.json')
+    capacity_test_configs = load_configs('configs/benchmark_tests/capacity_test/capacity_test_template_gd.json')
 
     test = CapacityTest(capacity_test_configs)
     test.run_test()
