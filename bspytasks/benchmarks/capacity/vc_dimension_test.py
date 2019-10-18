74ebd9ebb992e10b99a86ccc943d07322d93e23b
unai <u.alegre@utwente.nl>
2019-10-18 15:45:03 +0200

    Working for both genetic and gradient descent algorithms seamlessly

--- bspytasks/benchmarks/capacity/vc_dimension_test.py
+++ bspytasks/benchmarks/capacity/vc_dimension_test.py
@@ -11,128 +11,71 @@ If successful (measured by a threshold on the correlation and by the perceptron 
 import numpy as np
 from matplotlib import pyplot as plt
 from bspyalgo.algorithm_manager import get_algorithm
-from bspytasks.benchmarks.capacity.interface import VCDimTargetEncoder, VCDimInputEncoder
+from bspyproc.utils.pytorch import TorchUtils
 import bspyinstr.utils.waveform as waveform
 from bspytasks.utils.accuracy import perceptron
+from bspytasks.benchmarks.capacity.interface import VCDimDataManager
 
 
 class VCDimensionTest():
 
     def __init__(self, configs, excel_file):
+        self.algorithm_configs = configs['algorithm_configs']
         self.algorithm = get_algorithm(configs['algorithm_configs'])
         self.output_dir = configs['results_base_dir']
         self.test_data_plot_name = '_plot.png'
         self.excel_file = excel_file
-        # self.targets_encoder = VCDimTargetEncoder(configs['encoder']['waveform'])
-        # self.inputs_encoder = VCDimInputEncoder(configs['encoder']['waveform'])
         self.threshold_parameter = configs['threshold_parameter']
         self.show_plots = configs['show_plots']
         self.amplitude_lengths = configs['encoder']['waveform']['amplitude_lengths']
         self.slope_lengths = configs['encoder']['waveform']['slope_lengths']
-        # if configs['algorithm_configs']['algorithm'] == 'genetic':
-        #     self.accuracy = perceptron
-        # elif configs['algorithm_configs']['algorithm'] == 'gradient_descent':
-
-    def generate_test_inputs(self, vc_dimension):
-        # @todo create a function that automatically generates non-linear inputs
-        # try:
-        if vc_dimension == 4:
-            return [[-0.7, -0.7, 0.7, 0.7], [0.7, -0.7, 0.7, -0.7]]  # Dimensions x Samples
-        elif vc_dimension == 5:
-            return [[-0.7, -0.7, 0.7, 0.7, -0.35],
-                    [0.7, -0.7, 0.7, -0.7, 0.0]]
-        elif vc_dimension == 6:
-            return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35],
-                    [0.7, -0.7, 0.7, -0.7, 0.0, 0.0]]
-        elif vc_dimension == 7:
-            return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35, 0.0],
-                    [0.7, -0.7, 0.7, -0.7, 0.0, 0.0, 1.0]]
-        elif vc_dimension == 8:
-            return [[-0.7, -0.7, 0.7, 0.7, -0.35, 0.35, 0.0, 0.0],
-                    [0.7, -0.7, 0.7, -0.7, 0.0, 0.0, 1.0, -1.0]]
-        # else:
-            #         raise VCDimensionException()
-            # except VCDimensionException:
-            # print('Dimension Exception occurred. The selected VC Dimension is %d Please insert a value between ' % vc_dimension)
-
-    def generate_binary_labels(self, vc_dimension):
-        # length of list, i.e. number of binary targets
-        binary_target_no = 2**vc_dimension
-        assignments = []
-        list_buf = []
-
-        # construct assignments per element i
-        print('===' * vc_dimension)
-        print('ALL BINARY LABELS:')
-        level = int((binary_target_no / 2))
-        while level >= 1:
-            list_buf = []
-            buf0 = [0] * level
-            buf1 = [1] * level
-            while len(list_buf) < binary_target_no:
-                list_buf += (buf0 + buf1)
-            assignments.append(list_buf)
-            level = int(level / 2)
-
-        binary_targets = np.array(assignments).T
-        print(binary_targets)
-        print('===' * vc_dimension)
-        return binary_targets
+        if self.algorithm_configs['algorithm'] == 'gradient_descent' and self.algorithm_configs['processor']['platform'] == 'simulation':
+            self.accuracy = self.get_accuracy_for_torch
+        else:
+            self.accuracy = self.get_accuracy
+        self.data_manager = VCDimDataManager(configs)
 
     def init_test(self, vc_dimension):
         self.vc_dimension = vc_dimension
         self.threshold = self.calculate_threshold()
-        self.inputs = self.generate_test_inputs(vc_dimension)
-        self.binary_labels = self.generate_binary_labels(vc_dimension)
-        # self.targets_encoder.load_encoders(vc_dimension)
-        # self.inputs_encoder.load_encoders(vc_dimension)
+        self.readable_inputs, self.transformed_inputs = self.data_manager.get_inputs(vc_dimension)
+        self.readable_targets, self.transformed_targets = self.data_manager.get_targets(vc_dimension)
+        if self.algorithm_configs['algorithm'] == 'gradient_descent' and self.algorithm_configs['processor']['platform'] == 'simulation':
+            self.mask = None
+        else:
+            self.mask = waveform.generate_mask(self.readable_targets[1], self.amplitude_lengths, slope_lengths=self.slope_lengths)  # Chosen readable_targets[1] because it might be better for debuggin purposes. Any other label or input could be taken.
 
         self.init_excel_file()
 
     def init_excel_file(self):
         column_names = ['gate', 'found', 'accuracy', 'best_output', 'control_voltages', 'correlation', 'best_performance']
-        self.excel_file.init_data(self.binary_labels, column_names)
+        self.excel_file.init_data(self.readable_targets, column_names)
 
     def calculate_threshold(self):
         return 1 - (self.threshold_parameter / self.vc_dimension)
 
     def run_test(self, binary_labels=np.array([])):
         if binary_labels.size == 0:
-            binary_labels = self.binary_labels
-        for label in binary_labels:
-            self.find_label(label)
+            binary_labels = self.readable_targets
+        for i in range(len(binary_labels)):
+            self.find_label(i)
 
     def get_not_found_gates(self):
         return self.excel_file.data['gate'].loc[self.excel_file.data['found'] == False]  # noqa: E712
 
-    def find_label(self, label):
-
+    def find_label(self, index):
+        label = self.readable_targets[index]
         if len(np.unique(label)) == 1:
             print('Label ', label, ' ignored')
             excel_results = self.ignore_label({})
         else:
             print('Finding classifier ', label)
-            # [:, :, np.newaxis]
-            inputs_waveform = np.array([])
-            for inp in self.inputs:
-                input_waveform = waveform.generate_waveform(inp, self.amplitude_lengths, slope_lengths=self.slope_lengths)
-                if inputs_waveform.shape == (0,):
-                    inputs_waveform = np.concatenate((inputs_waveform, input_waveform))
-                else:
-                    inputs_waveform = np.vstack((inputs_waveform, input_waveform))
-            # if len(inputs_waveform.shape) == 1:
-            #    inputs_waveform = inputs_waveform[:, np.newaxis]
-            inputs_waveform = inputs_waveform.T  # device_model --> (samples,dimension) ; device --> (dimensions,samples)
-            target_waveform = waveform.generate_waveform(label, self.amplitude_lengths, slope_lengths=self.slope_lengths)
-            target_waveform = target_waveform[:, np.newaxis]
-            mask = waveform.generate_mask(label, self.amplitude_lengths, slope_lengths=self.slope_lengths)
 
-            algorithm_data = self.algorithm.optimize(inputs_waveform, target_waveform, mask=mask)
-            # algorithm_data = self.algorithm.optimize(self.inputs_encoder.inputs, label)
+            algorithm_data = self.algorithm.optimize(self.transformed_inputs, self.transformed_targets[index], mask=self.mask)
 
             algorithm_data.judge()
             excel_results = algorithm_data.results
-            excel_results['accuracy'], _, _ = perceptron(algorithm_data.results['best_output'][mask], target_waveform[mask])
+            excel_results['accuracy'], _, _ = self.accuracy(algorithm_data.results['best_output'][algorithm_data.results['mask']], self.transformed_targets[index][algorithm_data.results['mask']])
             excel_results['found'] = excel_results['accuracy'] >= self.threshold
 
         excel_results['gate'] = label
@@ -143,6 +86,14 @@ class VCDimensionTest():
         # La correlacion tiene que ser super alta debe ser 95
 
         # row = {'gate': label, 'found': found}
+
+    def get_accuracy(self, best_output, target):
+        return perceptron(best_output, target)
+
+    def get_accuracy_for_torch(self, best_output, target):
+        # best_output = TorchUtils.get_numpy_from_tensor(best_output)
+        target = TorchUtils.get_numpy_from_tensor(target)
+        return perceptron(best_output, target)
 
     def ignore_label(self, excel_results):
         excel_results['control_voltages'] = np.nan
@@ -168,7 +119,7 @@ class VCDimensionTest():
         plt.plot(np.linspace(np.nanmin(fitness_classifier),
                              np.nanmax(fitness_classifier)),
                  self.threshold * np.ones_like(np.linspace(0, 1)), '-k')
-        plt.xlabel('Fitness')
+        plt.xlabel('Fitness / Performance')
         plt.ylabel('Accuracy')
         plt.savefig(self.output_dir + 'dimension_' + str(self.vc_dimension) + self.test_data_plot_name)
         if self.show_plots:
