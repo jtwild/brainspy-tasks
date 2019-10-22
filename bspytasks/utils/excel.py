6a4e08c6fa69b2e3453290250e65473b9d874aa6
unai <u.alegre@utwente.nl>
2019-10-22 13:36:08 +0200

    Added new function for adding columns. Corrected label issue when adding results.

--- bspytasks/utils/excel.py
+++ bspytasks/utils/excel.py
@@ -8,6 +8,9 @@ class ExcelFile():
     def init_data(self, index, column_names):
         self.data = pd.DataFrame(index=pd.Series(map(str, index)), columns=column_names)
 
+    def insert_column(self, column_name, column_data):
+        self.data[column_name] = pd.Series(column_data, index=self.data.index)
+
     def save_tab(self, tab_name, data=None):
         if data is None:
             aux = self.data
@@ -20,4 +23,3 @@ class ExcelFile():
 
     def add_result(self, label, results):
         self.data.loc[str(label)] = pd.Series(results)
-        # self.data.loc[label] = pd.Series(results)
