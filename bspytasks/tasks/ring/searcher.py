823cfa54120c1ffe6232c7ec883b43c941809c62
ualegre <u.alegre@utwente.nl>
2020-01-22 15:23:16 +0100

    Minor fix and minor improvements

--- bspytasks/tasks/ring/searcher.py
+++ bspytasks/tasks/ring/searcher.py
@@ -1,18 +1,12 @@
 from bspyalgo.utils.io import load_configs
 from bspytasks.tasks.ring.ring_classification import RingClassificationTask as Task
-import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from bspytasks.utils.excel import get_series_with_numpy
 
-RUNS = 20
+RUNS = 10
 
 task = Task(load_configs('configs/tasks/ring/template_gd_architecture_2.json'))
-for run in range(RUNS):
-    print(f'########### RUN {run} ################')
-    result = task.run_task(run=run)
-task.close_test()
 
 performance_per_run = np.zeros(RUNS)
 correlation_per_run = np.zeros(RUNS)
@@ -20,12 +14,11 @@ best_output_run = []
 
 for run in range(RUNS):
     print(f'########### RUN {run} ################')
-    excel_results = task.run_task(run=run)
+    excel_results, _ = task.run_task(run=run)
     performance_per_run[run] = excel_results["best_performance"]
     correlation_per_run[run] = excel_results["correlation"]
     best_output_run.append(excel_results['best_output'])
 
-task.close_test()
 
 best_index = np.argmin(performance_per_run)
 best_run = best_output_run[best_index]
