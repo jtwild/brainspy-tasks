4c3fdf0b9df95bdadc08c008e88eeac870faac26
ualegre <u.alegre@utwente.nl>
2020-02-06 16:58:22 +0100

    Minor fixes and general cleaning of the data loader for the ring classification

--- bspytasks/tasks/ring/data_loader.py
+++ bspytasks/tasks/ring/data_loader.py
@@ -3,6 +3,7 @@ from bspyproc.utils.waveform import generate_waveform, generate_mask
 from bspyproc.utils.pytorch import TorchUtils
 from bspyproc.utils.input import normalise, map_to_voltage
 
+# @todo: This data should come from the model
 MAX_INPUT_VOLT = np.asarray([0.6, 0.6, 0.6, 0.6, 0.6, 0.3, 0.3])
 MIN_INPUT_VOLT = np.asarray([-1.2, -1.2, -1.2, -1.2, -0.7, -0.7])
 
@@ -11,11 +12,6 @@ class RingDataLoader():
 
     def __init__(self, configs):
         self.configs = configs
-
-    # def get_max_input_voltages(input_indices):
-    #     MAX_INPUT_VOLT[input_indices]
-
-    # def get_min_input_voltages(configs):
 
     def get_data(self, processor_configs):
         with np.load(self.configs['ring_data_path']) as data:
@@ -40,11 +36,10 @@ class RingDataLoader():
             inputs[:, i] = map_to_voltage(inputs[:, i],
                                           MIN_INPUT_VOLT[processor_configs['input_indices'][i]],
                                           MAX_INPUT_VOLT[processor_configs['input_indices'][i]])
-            inputs[:, i] = generate_waveform(inputs[:, i], processor_configs['waveform']['amplitude_lengths'], slope_lengths=processor_configs['waveform']['slope_lengths'])
-
+        inputs = self.generate_data_waveform(inputs, processor_configs['waveform']['amplitude_lengths'], processor_configs['waveform']['slope_lengths'])
         if processor_configs["platform"] == 'simulation' and processor_configs["network_type"] == 'dnpu':
             return TorchUtils.get_tensor_from_numpy(inputs)
-        return inputs.T
+        return inputs
 
     def process_targets(self, targets, processor_configs):
         mask = (targets == 1)
@@ -55,38 +50,13 @@ class RingDataLoader():
             return TorchUtils.get_tensor_from_numpy(targets)
         return targets
 
-    # def get_ring_data_from_npz_2(self, processor_configs):
-    #     with np.load(self.configs['ring_data_path']) as data:
-    #         inputs = data['inp_wvfrm'][::self.configs['steps'], :]  # .T
-    #         print('Input shape: ', inputs.shape)
-    #         targets = data['target'][::self.configs['steps']]
-    #         print('Target shape ', targets.shape)
-    #     return self.process_npz_targets_2(inputs, targets, processor_configs=processor_configs)
+    def generate_data_waveform(self, data, amplitude_lengths, slope_lengths):
+        data_waveform = np.array([])
+        for i in range(data.shape[1]):
+            d_waveform = generate_waveform(data[:, i], amplitude_lengths, slope_lengths=slope_lengths)
+            if data_waveform.shape == (0,):
+                data_waveform = np.concatenate((data_waveform, d_waveform))
+            else:
+                data_waveform = np.vstack((data_waveform, d_waveform))
 
-    # def process_data_2(self, inputs, targets, processor_configs):
-    #     # inputs = normalise(inputs)
-    #     targets[targets == 0] = 1
-    #     targets[targets == 1] = 0
-    #     amplitude_lengths = processor_configs['waveform']['amplitude_lengths']
-    #     slope_lengths = processor_configs['waveform']['slope_lengths']
-    #     mask = generate_mask(targets, amplitude_lengths, slope_lengths=slope_lengths)
-    #     # inputs = self.generate_data_waveform(inputs, amplitude_lengths, slope_lengths)
-    #     # targets = np.asarray(generate_waveform(targets, amplitude_lengths, slope_lengths)).T
-
-    #     if processor_configs["platform"] == 'simulation' and processor_configs["network_type"] == 'dnpu':
-    #         inputs = TorchUtils.get_tensor_from_numpy(inputs)
-    #         targets = TorchUtils.get_tensor_from_numpy(targets)
-
-    #     return inputs, targets, mask
-
-    # def generate_data_waveform(self, data, amplitude_lengths, slope_lengths):
-    #     data_waveform = np.array([])
-    #     for i in range(data.shape[1]):
-    #         d_waveform = generate_waveform(data[:, i], amplitude_lengths, slope_lengths=slope_lengths)
-    #         if data_waveform.shape == (0,):
-    #             data_waveform = np.concatenate((data_waveform, d_waveform))
-    #         else:
-    #             data_waveform = np.vstack((data_waveform, d_waveform))
-    #     # if len(inputs_waveform.shape) == 1:
-    #     #    inputs_waveform = inputs_waveform[:, np.newaxis]
-    #     return data_waveform.T  # device_model --> (samples,dimension) ; device --> (dimensions,samples)
+        return data_waveform.T
