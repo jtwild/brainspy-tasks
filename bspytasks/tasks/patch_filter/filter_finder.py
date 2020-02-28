9478102c5306f6f102fa2f03f9e8218adc9bfeaf
Jochem <j.t.wildeboer@student.utwente.nl>
2020-02-28 11:50:23 +0100

    initial working setup for saving. ADjustmenets are still needed to correctly pass the is_main flag and save in the correct directories.

--- bspytasks/tasks/patch_filter/filter_finder.py
+++ bspytasks/tasks/patch_filter/filter_finder.py
@@ -28,29 +28,29 @@ class FilterFinder():
         self.configs = configs
         self.excel_file = None    # Not sure why it is done like this, but I copied the structure defined by Unai in capacity test
         self.is_main = is_main
-        self.input_dim = len( configs['filter_finder']['algorithm_configs']['processor']['input_indices'] )
+        self.input_dim = len( configs['algorithm_configs']['processor']['input_indices'] )
         self.init_dirs(self.input_dim)  # Initialize directory and excel file.
-
+        self.init_excel_file()
         # And load other relevant config parameters
         self.load_methods(self.configs)
-        if 'validation' in self.configs['filter_finder']:
+        if 'validation' in self.configs:
             raise Warning('Validation not implemented. Ignoring!')
-        self.max_attempts = self.configs['filter_finder']['max_attempts']
-        self.show_plots = self.configs['filter_finder']['show_plots']
-        self.algorithm = get_algorithm(configs['filter_finder']['algorithm_configs'])  # An instance of GD or GA, loading all algorithm related parameters.
-
-
+        self.max_attempts = self.configs['max_attempts']
+        self.show_plots = self.configs['show_plots']
+        self.algorithm = get_algorithm(configs['algorithm_configs'])  # An instance of GD or GA, loading all algorithm related parameters.
+        self.algorithm.init_dirs(self.base_dir)
+        self.save_plot = self.configs['save_plot']
         # Check if correct loss/fitness function is defined
-        #if self.configs['filter_finder']['algorithm_configs']['algorithm'] == 'gradient_descent':
+        #if self.configs['algorithm_configs']['algorithm'] == 'gradient_descent':
         #    key = 'loss_function'
-        #elif self.configs['filter_finder']['algorithm_configs']['algorithm'] == 'genetic':
+        #elif self.configs['algorithm_configs']['algorithm'] == 'genetic':
         #    key = 'fitness_function_type'
         #else:
         #    raise ValueError('Selected algorithm not tested/implemented.')
-        #if configs['filter_finder']['algorithm_configs']['hyperparameters'][key] != 'sigmoid_distance':
+        #if configs['algorithm_configs']['hyperparameters'][key] != 'sigmoid_distance':
         #    raise ValueError('For now, only implemented with sigmoidal distance loss/fitness function.')
         #else:
- #               self.algorithm = get_algorithm(configs['filter_finder']['algorithm_configs'])  # An instance of GD or GA, loading all algorithm related parameters.
+ #               self.algorithm = get_algorithm(configs['algorithm_configs'])  # An instance of GD or GA, loading all algorithm related parameters.
 
 
 
@@ -58,7 +58,7 @@ class FilterFinder():
     def init_dirs(self, input_dim):
         results_folder_name = f'patch_filter_{input_dim}_points'
         file_name = "patch_filter_results.xlsx"
-        self.base_dir = self.configs['filter_finder']['results_base_dir']
+        self.base_dir = self.configs['results_base_dir']
         if self.is_main:
             base_dir = create_directory_timestamp(self.base_dir, results_folder_name)
             self.excel_file = ExcelFile(os.path.join(base_dir, file_name))
@@ -66,14 +66,20 @@ class FilterFinder():
             if self.excel_file is None:
                 self.excel_file = ExcelFile(os.path.join(self.base_dir, file_name))
             base_dir = os.path.join(self.base_dir, results_folder_name)
-        self.configs['filter_finder']['results_base_dir'] = base_dir
+        self.configs['results_base_dir'] = base_dir
         self.configs_dir = os.path.join(self.base_dir, 'test_configs.json')
         return base_dir
 
-    def load_task_configs(self, configs):
+    def init_excel_file(self):
+        #column_names = ['number of input points','minimum seperation','average seperation','minimum current', 'maximum current','input electrodes','control electrodes','control voltages','input voltages','output currents']
+        self.excel_file.init_data([''])
+        self.excel_file.reset()
+        #self.excel_file.insert_column('number of input points', self.input_dim)
+        #self.excel_file.insert_column('input electrodes', '')
+        #self.excel_file.insert_column('control electrodes', '')
 
     def load_methods(self, configs):
-        if configs['filter_finder']['algorithm_configs']['processor']['platform'] == 'simulation':
+        if configs['algorithm_configs']['processor']['platform'] == 'simulation':
             self.find_filter_core = self.optimize
         else:
             raise ValueError('Algorithm or processor not yet implemented')
@@ -87,32 +93,35 @@ class FilterFinder():
         excel_results = algorithm_data.results
         return excel_results
 
-    def plot_best_filter(self, excel_results, show_plots=True):
-        performance = []
-        for results in excel_results:
-            performance.append( np.nanmin( results['performance_history'] ) )
-        best_attempt_index = np.argmin(performance)
-
+    def plot_best_filter(self, excel_results, index, show_plots=True, save_plot=True):
+        # Setup axes
         fig, axs = plt.subplots(1,2, sharey=False)
-        y = excel_results[best_attempt_index]['best_output']
-        inputs = excel_results[best_attempt_index]['inputs'].tolist()
+        y = excel_results[index]['best_output']
+        inputs = excel_results[index]['inputs'].tolist()
+
+        # Plot lines in output current
         for i in range( len(y) ):
             axs[0].plot([0,1], [y[i], y[i]] )
             text = ''.join(str(round(e,2))+', ' for e in inputs[i] )
             axs[0].annotate(text, [0,y[i]])
-        # and a plot of the loss over time
-        axs[1].plot(excel_results[best_attempt_index]['performance_history'])
+        # Pplot of the loss over time
+        axs[1].plot(excel_results[index]['performance_history'])
+
+        #Formatting:
         axs[1].grid(which='both')
         axs[0].set_ylabel('Current (nA)')
         axs[0].grid(which='major')
         axs[0].set_xticks([])
-
         fig.suptitle(f"Best output for input dimension {len(inputs[0])} \n"
-                     f"Minimum nearest neighbour distance: {np.round( excel_results[best_attempt_index]['dist']['min'], 4)} \n"
-                     f"Control voltages: {str(np.round(excel_results[best_attempt_index]['control_voltages'][0],3))} V")
-        #TODO: add ticklabels defining the inputs
-        #excel_results['inputs']int().tolist() can be used to extract the inputs in a python list.
+                     f"Minimum nearest neighbour distance: {np.round( excel_results[index]['dist']['min'], 4)} \n"
+                     f"Control voltages: {str(np.round(excel_results[index]['control_voltages'][0],3))} V")
 
+        # Show if required
+        if show_plots:
+            plt.show()
+        # Save plot if required:
+        if save_plot:
+            plt.savefig( os.path.join(self.base_dir, 'best_output.pdf') )
 
 # find_filter is the main method. All else above is required for this function
     def find_filter(self):
@@ -132,27 +141,37 @@ class FilterFinder():
         print('--------------------------------------------------------------------')
         print(f'       Patch Filter Finder with {str(self.input_dim)} points        ')
         print('--------------------------------------------------------------------')
-        excel_results = []
+        self.excel_results = []
         for attempt in range(self.max_attempts):
             print(f'\nAttempt {attempt} of {self.max_attempts}.')
-            excel_results.append( self.find_filter_core(inputs) )
-            distances = excel_results[attempt]['best_output'] - excel_results[attempt]['best_output'].T
+            self.excel_results.append( self.find_filter_core(inputs) )
+            distances = self.excel_results[attempt]['best_output'] - self.excel_results[attempt]['best_output'].T
             np.fill_diagonal(distances, np.nan)  # ignore diagonal, distance to itself always zero
-            distance_nearest = np.nanmin( abs(distances) )
-            excel_results[attempt]['dist'] = dict()  # intialize new key
-            excel_results[attempt]['dist']['nn'] = distance_nearest  # all nearest neighbour distances
-            excel_results[attempt]['dist']['avg'] = np.mean( distance_nearest )  # average nearest neighbour distance
-            excel_results[attempt]['dist']['min'] = np.nanmin( distance_nearest )  # minimal nearest neighbour distanc
+            distance_nearest = np.nanmin( abs(distances), axis=0 )
+            self.excel_results[attempt]['dist'] = dict()  # intialize new key
+            self.excel_results[attempt]['dist']['nn'] = distance_nearest  # all nearest neighbour distances
+            self.excel_results[attempt]['dist']['avg'] = np.mean( distance_nearest )  # average nearest neighbour distance
+            self.excel_results[attempt]['dist']['min'] = np.nanmin( distance_nearest )  # minimal nearest neighbour distanc
+            self.excel_file.add_result(self.excel_results[attempt])
+        # Find best attempt:
+        performance = []
+        for results in self.excel_results:
+            performance.append( np.nanmin( results['performance_history'] ) )
+        best_attempt_index = np.argmin(performance)
 
-        # Save data if required
+        # Save data
+        aux = self.excel_file.data.copy()
+        tab_name = 'Filter_test'
+        self.excel_file.save_tab(tab_name, data=aux)
+        self.excel_file.close_file()
 
         # Plot best output
-        self.plot_best_filter(excel_results, self.show_plots)
-        return excel_results
+        self.plot_best_filter(self.excel_results, best_attempt_index, self.show_plots, self.save_plot)
+        return self.excel_results
 
 #%% Testing
 if __name__ == '__main__':
     from bspyalgo.utils.io import load_configs
     configs = load_configs('configs/tasks/filter_finder/template_ff_gd.json')
-    task = FilterFinder(configs, is_main=True) #initialize class
+    task = FilterFinder(configs['filter_finder'], is_main=True) #initialize class
     excel_results = task.find_filter()
