2bdcd00304ac4df73be3cd0cb94cfa2aef04b190
Jochem <j.t.wildeboer@student.utwente.nl>
2020-04-06 17:28:20 +0200

    minor updates. - removed unnecessary import of packages. - added a ranking, via a function that can also be called for other values to be ranked. - plotted ranking as barplots

--- bspytasks/validation/electrode_ranking/perturbation.py
+++ bspytasks/validation/electrode_ranking/perturbation.py
@@ -9,9 +9,8 @@ Author: Jochem Wildeboer
 """
 from bspyalgo.utils.io import load_configs
 from bspysmg.model.data.outputs import test_model
-from bspysmg.model.data.outputs.train_model import train_surrogate_model
 import numpy as np
-from bspyproc.bspyproc import get_processor
+import matplotlib.pyplot as plt  # for barplot
 
 
 def perturb_data(configs, steps=1):
@@ -25,9 +24,10 @@ def perturb_data(configs, steps=1):
     for i in electrodes:
         amplitude = perturb_fraction * (inputs[:, i].max() - inputs[:, i].min())
         inputs[:, i] = inputs[:, i] + np.random.uniform(low=-amplitude / 2, high=+amplitude / 2, size=inputs[:, i].shape)
-    # Save perturbed data such that it can be read by the (existing) test_model.get_error
+    # Save perturbed data such that it can be read by the (existing) test_model.get_error(.) function
     np.savez(output_file, inputs=inputs, outputs=outputs, info=info)
     return inputs, outputs, info
+
 
 def get_perturbed_rmse(configs):
     # Load config data
@@ -38,12 +38,31 @@ def get_perturbed_rmse(configs):
     for i in range(len(perturb_fraction_sets)):
         configs['perturbation']['perturb_fraction'] = perturb_fraction_sets[i]
         for j in range(len(electrodes_sets)):
-            configs['perturbation']['electrodes']= electrodes_sets[j]
+            configs['perturbation']['electrodes'] = electrodes_sets[j]
             # Perturb data
             perturb_data(configs)
             # Get error
             rmse[i, j] = np.sqrt(test_model.get_error(configs['processor']["torch_model_dict"], configs["data"]['perturbed_data_file']))
     return rmse
+
+
+def rank_low_to_high(electrodes_sets, values, do_plot=False):
+    ranking_indices = np.argsort(-values)  # take negative of slope to order the slope from largest (most positive -> most negative) to smallest
+    ranking = electrodes_sets[ranking_indices]
+    ranked_values = values[ranking_indices]
+    # Plot ranking in barplot
+    if do_plot:
+        plt.figure()
+        plt.bar(range(len(electrodes_sets)), ranked_values)
+        for j in range(len(electrodes_sets)):
+            s = np.array2string(np.array(ranking[j]))
+            xy = [j, ranked_values[j]]
+            plt.annotate(s, xy)
+        plt.title(f"Ranking of different electrodes indicated by annotation\n \
+          Ranking: {np.array2string(ranking.flatten())}")
+        plt.xlabel('Ranking')
+    return ranking, ranked_values
+
 
 if __name__ == "__main__":
     # User variables
@@ -51,14 +70,14 @@ if __name__ == "__main__":
     rmse = get_perturbed_rmse(configs)
 
     # Visualize results
-    import matplotlib.pyplot as plt
-    electrodes_sets = configs['perturbation']['electrodes_sets']
-    perturb_fraction_sets = configs['perturbation']['perturb_fraction_sets']
+    electrodes_sets = np.array(configs['perturbation']['electrodes_sets'])
+    perturb_fraction_sets = np.array(configs['perturbation']['perturb_fraction_sets'])
     plt.figure()
     for j in range(len(electrodes_sets)):
         plt.plot(perturb_fraction_sets, rmse[:, j], marker='s', linestyle='')  # or use plt.semilogy(..)
     plt.xlabel('Perturbation fraction')
     plt.ylabel('RMSE (nA)')
+    plt.title('RMSE scaling: simulated (square markers) and linear fit (solid line)')
     #legend_entries = (np.array(['Electrode']*len(electrodes_sets)).flatten().astype(str) + np.array(electrodes_sets).flatten().astype(str) ).tolist()
     plt.legend(electrodes_sets)
     plt.grid()
@@ -66,6 +85,7 @@ if __name__ == "__main__":
     # Fitting linear
     from sklearn import linear_model
     plt.gca().set_prop_cycle(None)  # reset color cycle, such that we have the same colors for the fitted lines as for the markers
+    linear_params = np.zeros([2, len(electrodes_sets)])  # to save intercept and slope of linear fit
     for j in range(len(electrodes_sets)):
         y = rmse[:, j]
         X = np.c_[perturb_fraction_sets]
@@ -74,5 +94,9 @@ if __name__ == "__main__":
         clf = linear_model.LinearRegression(fit_intercept=True).fit(X, y, sample_weight)
         X_test = np.c_[np.arange(min(perturb_fraction_sets), max(perturb_fraction_sets), 0.001)]
         plt.plot(X_test, clf.predict(X_test))
+        # Save intercept and linear slope
+        linear_params[0, j], linear_params[1, j] = clf.intercept_, clf.coef_[0]
 
-    plt.title('RMSE scaling: simulated (square markers) and linear fit (solid line)')
+    # Ranking electrode importance
+    ranking, ranked_values = rank_low_to_high(electrodes_sets, linear_params[1,:], do_plot=True)
+    plt.ylabel('Slope (RMSE / noise fraction)')
