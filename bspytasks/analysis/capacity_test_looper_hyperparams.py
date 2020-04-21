4f9618cb051df7c2f38b22da7912d7b8dd1c253d
Jochem <jochemsaccounts@hotmail.com>
2020-04-21 17:57:40 +0200

    new files used for hyperparams looping

---
+++ bspytasks/analysis/capacity_test_looper_hyperparams.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 12:47:22 2020
+
+@author: Jochem
+"""
+# %% Import packages
+from bspytasks.benchmarks.capacity.capacity_test import CapacityTest
+from bspyalgo.utils.io import load_configs
+import numpy as np
+import matplotlib.pyplot as plt
+# %% User data
+base_configs = load_configs('configs/benchmark_tests/capacity/template_gd.json')
+output_directory = r"C:/Users/Jochem/Desktop/2020_04_21_capacity_loop_hyperparameters/"
+# Lists to loop over
+max_attempts_list = [5, 10, 30]
+learning_rate_list = [0.01, 0.001, 0.0001]
+loss_function_list = ['corrsig', 'bce']
+nr_epochs_list = [250, 750, 1250]
+
+# %% Loop over all defined lists
+base_configs['capacity_test']['results_base_dir'] = output_directory
+descrips = np.empty([3,3,2,3], dtype=str)
+capacities = np.full([3,3,2,3], np.nan)
+summaries = np.empty([3,3,2,3], dtype=object)
+for i in range(len(max_attempts_list)):
+    for j in range(len(learning_rate_list)):
+        for k in range(len(loss_function_list)):
+            for l in range(len(nr_epochs_list)):
+                configs = base_configs.copy()
+                # Edit the configs
+                configs['capacity_test']['vc_dimension_test']['boolean_gate_test']['max_attempts'] = max_attempts_list[i]
+                configs['capacity_test']['vc_dimension_test']['boolean_gate_test']['algorithm_configs']['hyperparameters']['learning_rate'] = learning_rate_list[j]
+                configs['capacity_test']['vc_dimension_test']['boolean_gate_test']['algorithm_configs']['hyperparameters']['loss_function'] = loss_function_list[k]
+                configs['capacity_test']['vc_dimension_test']['boolean_gate_test']['algorithm_configs']['hyperparameters']['nr_epochs'] = nr_epochs_list[l]
+
+                # Start the test
+                test = CapacityTest(configs['capacity_test'])
+                test.run_test(validate=False)
+                plt.close('all')
+                
+                # Save some extra results
+                descrips[i,j,k,l] = f"max_attempts: {max_attempts_list[i]}, learning_rate: {learning_rate_list[j]}, loss_function: {loss_function_list[k]}, nr_epochs: {nr_epochs_list[l]}" 
+                capacities[i,j,k,l] = test.summary_results['capacity_per_N'][0]
+                summaries[i,j,k,l] = test.summary_results
+np.savez(output_directory + 'loop_items.npz', capacities=capacities, summaries = summaries, descrips = descrips, 
+         max_attempts_list = max_attempts_list, learning_rate_list = learning_rate_list, 
+         loss_function_list = loss_function_list, nr_epochs_list = nr_epochs_list)
