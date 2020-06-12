6f161f86387d7e91749b9d17a477aab431973611
Jochem <j.t.wildeboer@student.utwente.nl>
2020-06-12 14:39:01 +0200

    two more plots adjusted to new data format

--- bspytasks/analysis/electrode_ranking/ranking_comparison.py
+++ bspytasks/analysis/electrode_ranking/ranking_comparison.py
@@ -19,21 +19,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import bspytasks.analysis.electrode_ranking.ranking_utils as rank_utils
-import os
 def getRowValues(dataframe, index_key):
     assert isinstance(dataframe, pd.DataFrame)
     index = dataframe.index.names.index(index_key)
 #    return dataframe.index.levels[index] # this can be used and is faster than ..unique() approach, but it changes the order to alphabetical! Which we do not want.
     return dataframe.index.get_level_values(index).unique()
 
-# %% Defining user variables locations
+# %% Defining user variables file locations and selections of what to plot
 scores = pd.read_pickle(r'C:\Users\Jochem\STACK\Daily_Usage\Bestanden\UT\TN_MSc\Afstuderen\Results\Electrode_importance\2020_04_29_Models_Electrodes_Comparison\results_dataframes\scores_new_brains.pkl')
 input_elecs = getRowValues(scores, 'input_elec')
 input_intervals = getRowValues(scores, 'input_interval')
 input_interval = 'full' # select this interval for plotting.
 models = getRowValues(scores, 'model')
-vcX = 'vc8' # select this vc dimension for plotting
+vcX = 'vc7' # select this vc dimension for plotting
 methods = ['grad', 'pert', vcX]
+
 # Manually invert perturbation score, because it seems to be inversely correlated
 scores.loc[:, 'pert'] = -scores.loc[:, 'pert']
 print('Perturbation score inverted')
@@ -78,30 +78,33 @@ for i, model in enumerate(models):
 fig_b, ax_b = plt.subplots(nrows=2, ncols=4, sharey=False)
 fig_b.suptitle('Normalized score vs electrode for every model')
 ax_b = ax_b.flatten()
-for i in range(n_models):
-    data = np.stack((norm_gradient[:, 0, i],
-                     norm_perturbation[:, 0, i],
-                     norm_vcX[:, 0, i]),
+for i, model in enumerate(models):
+    df_filter = (slice(None), input_interval, model) # this selects all data generated for one model.
+    data = np.stack((scores.loc[df_filter, methods[0]+'_norm'].values,
+                     scores.loc[df_filter, methods[1]+'_norm'].values,
+                     scores.loc[df_filter, methods[2]+'_norm'].values),
                     axis=0)
-    legend = descr_methods
-    xticklabels = descr_elec
+    legend = methods[0:3]
+    xticklabels = input_elecs
     ax = ax_b[i]
     rank_utils.bar_plotter_2d(data, legend, xticklabels, ax=ax, sort_index=2)
-    ax.set_title(descr_models_short[i])
+    ax.set_title(models[i])
     ax.set_xlabel('Electrode #')
     ax.set_ylabel('Normalized score')
 
 # %% comparable as above with normalized data, bu then plotting in a 2d plane
 fig_b, ax_b = plt.subplots(nrows=2, ncols=4, sharey=False)
+fig_b.suptitle('XY plot of vcX versus gradient/perturbation')
 ax_b = ax_b.flatten()
-for i in range(n_models):
-    x_data = vcX[:, 0, i]
-    y_data1, color1 = perturbation[:, 0, i], 'blue'
-    y_data2, color2 = gradient[:,0,i], 'red'
-    xticklabels = descr_elec
+for i, model in enumerate(models):
+    df_filter = (slice(None), input_interval, model)
+    x_data = scores.loc[df_filter, vcX]
+    y_data1, color1 = scores.loc[df_filter, 'pert'], 'blue'       #perturbation[:, 0, i], 'blue'
+    y_data2, color2 = scores.loc[df_filter, 'grad'], 'red'      #gradient[:,0,i], 'red'
+    xticklabels = input_elecs
     ax = ax_b[i]
-    ax.set_title(descr_models_short[i])
-    ax.set_xlabel('vcX capacity')
+    ax.set_title(models[i])
+    ax.set_xlabel(vcX+' capacity')
     # Plot 1
     ax.plot(x_data, y_data1, linestyle='None', marker='+')
     ax.set_ylabel('Perturbation score', color=color1)
@@ -111,6 +114,28 @@ for i in range(n_models):
     ax_twin.plot(x_data, y_data2, color=color2, linestyle='None', marker='+')
     ax_twin.set_ylabel('Gradient score', color=color2)
     ax_twin.tick_params(axis='y', color=color2)
+
+# FIll the lsat plot with a combination of all data
+#model_selection = ('brains2.1','brains2.2')
+#model_selection = ('darwin2','darwin3')
+model_selection = slice(None)
+df_selection = (slice(None), input_interval, model_selection)
+x_data = scores.loc[df_selection, vcX]
+y_data1, color1 = scores.loc[df_selection, 'pert'], 'blue'
+y_data2, color2 = scores.loc[df_selection, 'grad'], 'red'
+ax = ax_b[7]
+xticklabels = input_elecs
+ax.set_title(model_selection)
+ax.set_xlabel(vcX+'capacity')
+# Plot 1
+ax.plot(x_data, y_data1, linestyle='None', marker='+')
+ax.set_ylabel('Perturbation score', color=color1)
+ax.tick_params(axis='y', color=color1)
+# Plot 2
+ax_twin = ax.twinx()
+ax_twin.plot(x_data, y_data2, color=color2, linestyle='None', marker='+')
+ax_twin.set_ylabel('Gradient score', color=color2)
+ax_twin.tick_params(axis='y', color=color2)
 
 # %% one subplot per method to compare if different models agree on the importantness of the device.
 # QUestion that can be answered by this plot: Which electrode is most important according ot this method, and do the different models agree?
