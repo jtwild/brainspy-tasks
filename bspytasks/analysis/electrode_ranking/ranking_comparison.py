4d23d3ad87949fb5e98c8c8b71c141def911bff1
Jochem <j.t.wildeboer@student.utwente.nl>
2020-06-15 17:13:12 +0200

    customizable subplot size

--- bspytasks/analysis/electrode_ranking/ranking_comparison.py
+++ bspytasks/analysis/electrode_ranking/ranking_comparison.py
@@ -25,14 +25,20 @@ def getRowValues(dataframe, index_key):
 #    return dataframe.index.levels[index] # this can be used and is faster than ..unique() approach, but it changes the order to alphabetical! Which we do not want.
     return dataframe.index.get_level_values(index).unique()
 
-# %% Defining user variables file locations and selections of what to plot
-scores = pd.read_pickle(r'C:\Users\Jochem\STACK\Daily_Usage\Bestanden\UT\TN_MSc\Afstuderen\Results\Electrode_importance\2020_04_29_Models_Electrodes_Comparison\results_dataframes\scores_new_brains.pkl')
+# %% User variables
+scores_file = r'C:\Users\Jochem\STACK\Daily_Usage\Bestanden\UT\TN_MSc\Afstuderen\Results\Electrode_importance\2020_04_29_Models_Electrodes_Comparison\results_dataframes\scores_new_brains.pkl'
+n_plot_rows = 2
+n_plot_cols = 3
+# %%Load plot variables. POssible user selection
+scores = pd.read_pickle(scores_file)
+scores = scores.drop(labels='darwin1',level='model') # drop darwin1 because the model is bad
 input_elecs = getRowValues(scores, 'input_elec')
 input_intervals = getRowValues(scores, 'input_interval')
 input_interval = 'full' # select this interval for plotting.
-models = getRowValues(scores, 'model')
+models = getRowValues(scores, 'model') # all models
 vcX = 'vc7' # select this vc dimension for plotting
 methods = ['grad', 'pert', vcX]
+#methods = ['vc2','vc3','vc4','vc5','vc6','vc7','vc8']
 
 # Manually invert perturbation score, because it seems to be inversely correlated
 scores.loc[:, 'pert'] = -scores.loc[:, 'pert']
@@ -63,7 +69,7 @@ for model in models:
 # %% comparing methods and ranking: 7 subplots (one per device). y-axis rank, x-axis electrodes, bars for methods (so 3 bars per electrodes)
 sort_index = 2
 # Question to be answered by this plot: Which electrode is most important for a specific device, and do the methods agree?
-fig_a, ax_a = plt.subplots(nrows=2, ncols=4, sharey=False)
+fig_a, ax_a = plt.subplots(nrows=n_plot_rows, ncols=n_plot_cols, sharey=False)
 fig_a.suptitle('Rank vs electrode for every model')
 ax_a = ax_a.flatten()
 for i, model in enumerate(models):
@@ -81,7 +87,7 @@ for i, model in enumerate(models):
     ax.set_ylabel('Rank by method')
 
 # %% same as above, but then on the normalized data.
-fig_b, ax_b = plt.subplots(nrows=2, ncols=4, sharey=False)
+fig_b, ax_b = plt.subplots(nrows=n_plot_rows, ncols=n_plot_cols, sharey=False)
 fig_b.suptitle('Normalized score vs electrode for every model')
 ax_b = ax_b.flatten()
 for i, model in enumerate(models):
@@ -99,7 +105,7 @@ for i, model in enumerate(models):
     ax.set_ylabel('Normalized score')
 
 # %% comparable as above with normalized data, bu then plotting in a 2d plane
-fig_b, ax_b = plt.subplots(nrows=2, ncols=4, sharey=False)
+fig_b, ax_b = plt.subplots(nrows=n_plot_rows, ncols=n_plot_cols, sharey=False)
 fig_b.suptitle('XY plot of vcX versus gradient/perturbation')
 ax_b = ax_b.flatten()
 for i, model in enumerate(models):
@@ -121,31 +127,31 @@ for i, model in enumerate(models):
     ax_twin.set_ylabel('Gradient score', color=color2)
     ax_twin.tick_params(axis='y', color=color2)
 
-# FIll the lsat plot with a combination of all data
-#model_selection = ('brains2.1','brains2.2')
-#model_selection = ('darwin2','darwin3')
-model_selection = slice(None)
-df_selection = (slice(None), input_interval, model_selection)
-x_data = scores.loc[df_selection, vcX]
-y_data1, color1 = scores.loc[df_selection, 'pert'], 'blue'
-y_data2, color2 = scores.loc[df_selection, 'grad'], 'red'
-ax = ax_b[7]
-xticklabels = input_elecs
-ax.set_title(model_selection)
-ax.set_xlabel(vcX+'capacity')
-# Plot 1
-ax.plot(x_data, y_data1, linestyle='None', marker='+')
-ax.set_ylabel('Perturbation score', color=color1)
-ax.tick_params(axis='y', color=color1)
-# Plot 2
-ax_twin = ax.twinx()
-ax_twin.plot(x_data, y_data2, color=color2, linestyle='None', marker='+')
-ax_twin.set_ylabel('Gradient score', color=color2)
-ax_twin.tick_params(axis='y', color=color2)
+## FIll the lsat plot with a combination of all data
+##model_selection = ('brains2.1','brains2.2')
+##model_selection = ('darwin2','darwin3')
+#model_selection = slice(None)
+#df_selection = (slice(None), input_interval, model_selection)
+#x_data = scores.loc[df_selection, vcX]
+#y_data1, color1 = scores.loc[df_selection, 'pert'], 'blue'
+#y_data2, color2 = scores.loc[df_selection, 'grad'], 'red'
+#ax = ax_b[7]
+#xticklabels = input_elecs
+#ax.set_title(model_selection)
+#ax.set_xlabel(vcX+'capacity')
+## Plot 1
+#ax.plot(x_data, y_data1, linestyle='None', marker='+')
+#ax.set_ylabel('Perturbation score', color=color1)
+#ax.tick_params(axis='y', color=color1)
+## Plot 2
+#ax_twin = ax.twinx()
+#ax_twin.plot(x_data, y_data2, color=color2, linestyle='None', marker='+')
+#ax_twin.set_ylabel('Gradient score', color=color2)
+#ax_twin.tick_params(axis='y', color=color2)
 
 # %% one subplot per method to compare if different models agree on the importantness of the device.
 # QUestion that can be answered by this plot: Which electrode is most important according ot this method, and do the different models agree?
-fig_c, ax_c = plt.subplots(nrows=1, ncols=3, sharey=False)
+fig_c, ax_c = plt.subplots(nrows=1, ncols=n_methods, sharey=False)
 fig_c.suptitle('Electrode rank vs electrode nummer, bar color per model, subplot per method')
 ax_c = ax_c.flatten()
 for i, method in enumerate(methods):
@@ -162,7 +168,7 @@ for i, method in enumerate(methods):
 
 # %% One fig per eelectrode, showing ranking vs methods
 # Question to be answered: For a specific electrode, which method gives highest ranking, and do the models agree?
-fig_d, ax_d = plt.subplots(nrows=2, ncols=4, sharey=False)
+fig_d, ax_d = plt.subplots(nrows=n_plot_rows, ncols=n_plot_cols, sharey=False)
 fig_d.suptitle('Method vs rank, bar color per model, subplot per electrode')
 ax_d = ax_d.flatten()
 for i, input_elec in enumerate(input_elecs):
@@ -183,7 +189,7 @@ for i, input_elec in enumerate(input_elecs):
 
 # %% One figure per electrode, basically the inverse of above
 # Question to be answered: For a specific electrode, which model get's highest ranking, and do the methodes agree?
-fig_e, ax_e = plt.subplots(nrows=2, ncols=4, sharey=True)
+fig_e, ax_e = plt.subplots(nrows=n_plot_rows, ncols=n_plot_cols, sharey=True)
 fig_e.suptitle('Model vs rank, bar color per method, subplot per electrode')
 ax_e = ax_e.flatten()
 for i, input_elec in enumerate(input_elecs):
